c85226cfcecec81265503ca20cfda310d8326b68
Martin Ashby <martin@ashbysoft.com>
2021-04-05 15:46:42 +0100

    Added watch script for faster iteration

---
+++ build.sh
@@ -0,0 +1,15 @@
+tcc \
+	-g \
+	-I . \
+	-ljson-c \
+	-lssl \
+	-lcrypto \
+	-lsqlite3 \
+	-D MG_ENABLE_OPENSSL=1 \
+	-D MG_ENABLE_LOG=0 \
+	main.c \
+	mongoose.c \
+	simclist.c \
+	termbox.c \
+	utf8.c \
+	-o slack-term-c
---
+++ watch.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+function cleanup() {
+	kill $st_pid
+	kill $tail_pid
+}
+
+trap cleanup EXIT
+
+for (( ; ; ))
+do
+	if test-f "slack-term-c"; then
+		./build.sh 2>&1
+	fi	
+
+	kill $st_pid
+	alacritty -e ./slack-term-c&
+	st_pid=$!
+
+	kill $tail_pid
+	tail -f ./dbg.log&
+	tail_pid=$!
+
+	# Wait for file changes, clear the terminal, and build
+	inotifywait -q -r -e create,modify,move,delete *.h *.c && \
+	  echo -ne "\033c" && \
+	  ./build.sh 2>&1
+done
+
+# Re-exec ourselves for the next edit
+exec $0 $@
